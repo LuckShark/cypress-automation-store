6f801d8500a322649cd9e5bc212e55a72e7ac000
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-01-14 11:42:29 -0300

    final with assert

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -10,18 +10,21 @@ describe("Verifyng variables, cypress commands and jquery commands", () => {
  //       const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
  //       skincareLink.click();
         //this approach is not recommended cuz even though the lines of code work, if we start extending upon our tests, we can't guarantee that this specific logic will continue to work.
+        //Importante - dessa primeira maneira também não conseguimos garantir a ordem de execução. Então é necessário tomar cuidado.
 
         //RECOMMENDED APPROACH
         cy.get("a[href*='product/category&path=']").contains("Makeup").click()
  //       cy.get("a[href*='product/category&path=']").contains("Skincare").click()
 
-        // O CÓDIGO VAI FALHAR
+        // O CÓDIGO VAI FALHAR 
+        //essa não é a maneira correta de usar consts para extrair um conteúdo
  //     const header = cy.get("h1 .maintext"); //pega todos os h1 que tem uma classe (.) com 'maintext' escrito
    //   cy.log(header.text()) //.text() é um comando jquery -> VAI FALHAR O TESTE
 
         cy.get("h1 .maintext").then(($headerText) => { //usei o cyget para localizar o header; depois usei o then para colocar ele no próximo bloco de código
             const headerText = $headerText.text() //criei uma constante e usei um comando jquery para extrair o texto
             cy.log("Found header text: " + headerText) //só pra confirmar que funcionou, usei o cy.log para mostrar o texto
+            expect(headerText).is.eq('Makeup') //e a confirmação final, o assert
         })
 
     });
