3ed57d9859853c2d0d53c10c0b9f536b8badbfee
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-01-27 17:16:34 -0300

    iteration pt2

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -2,12 +2,22 @@
 
 
 describe("Iterate over elements", () => {
-    it("Log information", () => { 
+    it("Log information of all hair care products", () => { 
         cy.visit("https://automationteststore.com/")
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+
+        cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+            cy.log("Index: " + index + " : " + $el.text())
+        });
+
     });
-    it("Navigating to specific product pages", () => { 
+    it("Add specific product to basket", () => { 
         cy.visit("https://automationteststore.com/")
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+        cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+            if($el.text().includes('Curls to straight Shampoo')){
+                cy.wrap($el).click() //ficou assim pois estou misturando jQuery com cy commands
+            }
+        });
     });
 })
